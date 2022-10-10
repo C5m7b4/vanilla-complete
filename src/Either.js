f9a8c4324fec5dc9cf3e71223d6fe424e66b3d76
C5m7b4 <mbedingfield@dcrpos.com>
2022-10-10 11:43:38 -0500

    Start of real monads

---
+++ src/Either.js
@@ -0,0 +1,15 @@
+const Right = (x) => ({
+  chain: (f) => f(x),
+  map: (f) => Right(f(x)),
+  fold: (f, g) => g(x),
+  toString: `Right: ${x}`,
+});
+
+const Left = (x) => ({
+  chain: (f) => Left(x),
+  map: (f) => Left(x),
+  fold: (f, g) => f(x),
+  toString: `Left: ${x}`,
+});
+
+export { Left, Right };
